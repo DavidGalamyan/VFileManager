1d714d2feecac2c8a86080cede8eb3add5f9f520
Verm-V <Verm-V@yandex.ru>
2021-02-23 23:21:33 +0600

    Сделал заготовку с простейшим вводом

--- VFileManager/Program.cs
+++ VFileManager/Program.cs
@@ -10,6 +10,15 @@ namespace VFileManager
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("VFileManager");
+            Console.WriteLine("Enter your command");
+            bool isExit = false;
+            while(!isExit)
+            {
+                Console.Write(":>");
+                string input = Console.ReadLine();
+                if (input == "exit") isExit = true;
+            }
         }
     }
 }
